eaeda0214d7b7a5b1d0a0d7d3535e053a2ab4a08
SouHirawata <hirawatasou@gmail.com>
2022-11-14 02:58:27 +0900

    :sparkles:convert to pdf

--- capture.py
+++ capture.py
@@ -2,21 +2,13 @@ import datetime
 import os
 import time
 
+import img2pdf
 import pyautogui
+from PIL import Image
 
-# number of pages
-PAGES = int(input("Enter number of pages: "))
-# interval (sec)
-SPAN = 1
-# Initials of output folder
-FOLDERNAME = "output"
-# Initials of output file
-FILENAME = "capture"
 
-# make output folder
+# function to make output folder
 def make_folder():
-    # get current time
-    now = datetime.datetime.now()
     # make folder name
     foldername = FOLDERNAME + "_" + now.strftime("%Y%m%d%H%M%S")
     # make folder
@@ -24,6 +16,7 @@ def make_folder():
     # return folder name
     return foldername
 
+# function to capture screen
 def capture(foldername):
     for p in range(PAGES):
         # make file name
@@ -35,10 +28,35 @@ def capture(foldername):
         # interval
         time.sleep(SPAN)
 
+# function to convert png to jpg
+def convert(foldername):
+    # get file list
+    files = os.listdir(foldername)
+    # set extension
+    ext = ".png"
+    # convert
+    with open(PDFNAME,"wb") as f:
+        f.write(img2pdf.convert([Image.open(foldername + "/" + j).filename for j in os.listdir(foldername)if j.endswith(ext)]))
+
 if __name__ == "__main__":
+    # number of pages
+    PAGES = int(input("Enter number of pages: "))
+    # interval (sec)
+    SPAN = 1
+    # Initials of output folder
+    FOLDERNAME = "output"
+    # Initials of output file
+    FILENAME = "capture"
+    # get current time
+    now = datetime.datetime.now()
+    # PDF file name
+    PDFNAME = FOLDERNAME + now.strftime("%Y%m%d%H%M%S") + ".pdf"
+
     # make folder
     foldername = make_folder()
     # time to move screen
     time.sleep(5)
     # capture
     capture(foldername)
+    # convert to pdf
+    convert(foldername)
